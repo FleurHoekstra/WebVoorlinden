2b494ace47f1a625e4c4f79f0268fbb6c3f46054
FleurHoekstra <90203729+FleurHoekstra@users.noreply.github.com>
2021-10-06 22:38:14 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,30 @@
 // var uitButton = document.querySelector ('#darkmodeOff');
 var darkmodeButton = document.querySelector ('#darkmodeOn')
 
-function darkmodeOn() {
+function clickOn() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+darkmodeButton.addEventListener('click' , clickOn );
+
+// var aanButton = document.querySelector ('#lichtAan');
+// var uitButton = document.querySelector ('#lichtUit');
+// var overlayElement = document.querySelector('#overlay')
+
+// // aan/uitknop
+// function aanKnop() {
+//     console.log('schakel aan');
+//    overlayElement.classList.remove('overlayaan');
+// }
+
+// function uitKnop() {
+//     console.log('schakel uit');
+//     overlayElement.classList.add('overlayaan');
+// }
+
+// aanButton.addEventListener('click' , aanKnop );
+// uitButton.addEventListener('click' , uitKnop );
 
 
 
